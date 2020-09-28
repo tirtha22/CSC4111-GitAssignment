9dd2d29899d5a950d88df038e5c012f56ce5420d
Tirtha Acharjee <gf0981@wayne.edu>
2020-09-27 20:24:21 -0400

    Changing the greetings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+//Author: Tirtha Acharjee <gf0981@wayne.edu>
   
 // Header file for input output functions 
 #include<iostream>  
@@ -10,7 +10,7 @@ using namespace std;
 // where the execution of program begins 
 int main() 
 { 
-    // prints hello world 
+    // prints Testing git merge! 
     cout<<"Hello World!"; 
       
     return 0; 
